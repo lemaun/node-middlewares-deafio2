42c3ef1548f7e5689f60a821cda9ca0ac4831ba3
Leandro Lago da Silva <leandro@datacampo.com.br>
2023-01-05 11:39:05 -0300

    middlewares implemented

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => username === user.username);
+
+  if (!user) {
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const qtdTodos = user.todos.length;
+
+  if (!user.pro && qtdTodos >= 10) {
+    return response.status(403).json({error: 'Todos quantity exceeded for your free plan'});
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  if (!validate(id)) {
+    return response.status(400).json({error: 'The ID is not a UUID'});
+  }
+  
+  const user = users.find(user => username === user.username);
+  if (!user) {
+    return response.status(404).json({error: 'User not found'});
+  }
+  
+  const todo = user.todos.find(todo => todo.id == id);  
+  if (!todo) {
+    return response.status(404).json({error: 'Todo not found in this user'});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => id === user.id);
+
+  if (!user) {
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
